ae20777c47cd46815d99ca714c0041f77b59dfae
Johar M. Ashfaque <39216339+UKVeteran@users.noreply.github.com>
2018-12-30 22:58:45 +0000

    Add files via upload

---
+++ Generate Test Data for Linear Regression Problems.py
@@ -0,0 +1,9 @@
+from sklearn import datasets
+from matplotlib import pyplot as plt
+ 
+# Get regression data from scikit-learn
+x, y = datasets.make_regression(n_samples=20, n_features=1, noise=0.5)
+ 
+# Vizualize the data
+plt.scatter(x,y)
+plt.show()
